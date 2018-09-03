0c6fb3a6c74feba1d44e11acdb235a57abe2a5bf
patrickaburu1 <patrickaburu1@gmail.com>
2018-09-03 11:09:29 +0300

    changed timeout to 5 minutes

--- src/main/java/com/patrick/authtoken/config/AuthorizationServerConfig.java
+++ src/main/java/com/patrick/authtoken/config/AuthorizationServerConfig.java
@@ -25,7 +25,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	static final String SCOPE_READ = "read";
 	static final String SCOPE_WRITE = "write";
 	static final String TRUST = "trust";
-	static final int ACCESS_TOKEN_VALIDITY_SECONDS = 1*60;
+
+	/*token expiry time now set to 5 seconds*/
+	static final int ACCESS_TOKEN_VALIDITY_SECONDS = 1*60*5;
 	static final int FREFRESH_TOKEN_VALIDITY_SECONDS = 6*60*60;
 
 	@Autowired
